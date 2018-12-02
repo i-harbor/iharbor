678f9c3af7a84b5380b6e98dd82f0532ca9b598b
李云 <lixiaoyuner@qq.com>
2018-12-02 18:07:00 +0800

    配置由以表的形式存在

--- apps/evcloud/admin.py
+++ apps/evcloud/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import VMLimit, VMConfig, APIAuth, EvcloudVM
 # Register your models here.
+
+@admin.register(VMLimit)
+class VMLimitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'limit')
+    list_display_links = ('id', 'user', 'limit')
+    list_filter = ('user', 'limit')
+
+@admin.register(VMConfig)
+class VMConfigAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cpu', 'mem', 'time')
+    list_display_links = ('id', 'cpu', 'mem', 'time')
+    list_filter = ('cpu', 'mem', 'time')
+
+@admin.register(APIAuth)
+class APIAuthAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'name', 'pwd', 'flag')
+    list_display_links = ('id', 'url', 'name', 'pwd', 'flag')
+    list_filter = ('url', 'name', 'pwd', 'flag')
+
+@admin.register(EvcloudVM)
+class EvcloudVMAdmin(admin.ModelAdmin):
+    list_display = ('vm_id', 'user', 'created_time', 'end_time')
+    list_display_links = ('vm_id', 'user', 'created_time', 'end_time')
+    list_filter = ('vm_id', 'user', 'created_time', 'end_time')
--- apps/evcloud/views.py
+++ apps/evcloud/views.py
@@ -2,31 +2,45 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
-from .models import EvcloudVM
+from .models import EvcloudVM, VMLimit, VMConfig
 from .utils import evcloud_operations
-vms = evcloud_operations()
+
 # Create your views here.
 
 def evcloud_list(request):
     if request.method == "GET":
-        image_list = ['centos7 64bit', 'win10 64bit', 'centos6 64bit', 'winxp 32bit', 'fedora28 64bit']
+        try:
+            vms = evcloud_operations()
+            image_list = vms.get_image_list()
+            image_list_dict = {}
+            for image in image_list.values():
+                image_list_dict[image['id']] = image['name'] + ' ' + image['version']
+        except:
+            pass
+        #image_list = ['centos7 64bit', 'win10 64bit', 'centos6 64bit', 'winxp 32bit', 'fedora28 64bit']
         user = request.user
-        vm_list = EvcloudVM.objects.filter(user=user).values()
+        vm_list = EvcloudVM.objects.filter(user=user).filter(deleted=False).values()
         vm_list_dict = {}
         for i, vm in enumerate(vm_list):
-            vm['vm_id_display'] = vm['vm_id'][-6:]
-            vm['vm_image_display'] = image_list[int(vm['vm_image'])-1]
+            try:
+                vm['vm_image_display'] = image_list_dict[int(vm['vm_image'])]
+            except:
+                vm['vm_image_display'] = '服务出错'
             vm['created_time_display'] = vm['created_time'].strftime("%Y-%m-%d")
             vm['end_time_display'] = vm['end_time'].strftime("%Y-%m-%d")
-            #vm['status'] = vms.get_status(vm['vm_id'])
             vm_list_dict[i] = vm
         return render(request, 'evcloud_list.html', {'vm_list_dict':vm_list_dict})
     elif request.method == "POST":
+        vms = evcloud_operations()
         vm_id = request.POST.get('vm_id')
         vm_operate = int(request.POST.get('vm_operate'))
         if vm_operate == 4:
             code, e = vms.delete(vm_id)
-            status = 'ok'
+            status = 'delete'
+            if code == 200:
+                vm = EvcloudVM.objects.get(vm_id=vm_id)
+                vm.deleted = True
+                vm.save()
         elif vm_operate == 5:
             code, e = vms.create_vnc(vm_id)
             status = 'ok'
@@ -50,23 +64,42 @@ def evcloud_add(request):
     #print(request.method)
     user = request.user
     if request.method == "GET":
-        return render(request, 'evcloud_add.html')
+        try:
+            vms = evcloud_operations()
+            image_list = vms.get_image_list()
+        except:
+            image_list[0] = {'name': '服务出错'}
+            pass
+        config_list = VMConfig.objects.all().values()
+        config_list_dict = {}
+        for i, config in enumerate(config_list):
+            config_list_dict[i] = config
+        return render(request, 'evcloud_add.html', {'config_list_dict': config_list_dict,
+                                                    'image_list': image_list,
+                                                    })
 
     elif request.method == "POST":
+        try:
+            limit = VMLimit.objects.get(user=user).limit
+        except :
+            VMLimit.objects.create(user=user)
+            limit = VMLimit.objects.get(user=user).limit
         result = {}
         image = int(request.POST.get('image'))
-        configure = int(request.POST.get('configure'))
-        if configure == 1:
-            cpu, mem = 2, 2048
-        elif configure == 2:
-            cpu, mem = 4, 4096
-        else:
-            cpu, mem = 8, 8192
+        config_id = int(request.POST.get('configure'))
+        config = VMConfig.objects.get(id=config_id)
+        cpu = config.cpu
+        mem = config.mem
+        time = config.time * 30
         try:
+            vm_number = EvcloudVM.objects.filter(user=user).filter(deleted=False).count()
+            if vm_number >= limit:
+                raise Exception('the number of VM exceed limit')
+            vms = evcloud_operations()
             create_result = vms.create(image, cpu, mem)
             EvcloudVM.objects.create(vm_id=create_result['uuid'],
                                      user=user,
-                                     end_time=datetime.datetime.now()+datetime.timedelta(days=365),
+                                     end_time=datetime.datetime.now()+datetime.timedelta(days=time),
                                      vm_image=image,
                                      vm_cpu=cpu,
                                      vm_mem=mem,
@@ -76,6 +109,7 @@ def evcloud_add(request):
             result['code'] = 200
         except Exception as e:
             result['code'] = 400
+            print(e)
             result['error_text'] = str(e).encode('utf-8').decode('unicode_escape')
         return JsonResponse(data = result)
     else:
