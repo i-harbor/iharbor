663616f7395d89652595d128a4a90b8a0f78849f
issacc <8qllyhy@gmail.com>
2021-10-28 16:30:12 +0800

    fix(mq): fix can't use negative index in QuerySet
    use scripts.py to start sync tasks

---
+++ syncserver/scripts.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import django
+from tqdm import tqdm
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# 设置项目的配置文件 不做修改的话就是 settings 文件
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webserver.settings")
+django.setup()
+from syncserver.sync import sync_object
+from api.backup import AsyncBucketManager
+
+manager = AsyncBucketManager()
+
+
+def main():
+    bucket_id = 0
+    last_bucket_id = 0
+    while True:
+        try:
+            buckets = manager.get_need_async_bucket_queryset(bucket_id)
+            if not buckets:
+                break
+            for bucket in tqdm(buckets, desc="buckets from: {}".format(buckets[0].id)):
+                bucket_id = bucket.id
+                obj_id = 0
+                last_obj_id = 0
+                while True:
+                    try:
+                        objs = manager.get_need_async_objects_queryset(bucket, obj_id)
+                        if not objs:
+                            break
+                        for obj in tqdm(objs, desc="bucket: {}".format(str(bucket.id)), leave=False):
+                            obj_id = obj.id
+                            sync_object.delay(bucket.id, obj.id, bucket.name, obj.na)
+                    except Exception as err:
+                        if last_obj_id != obj_id:
+                            last_obj_id = obj_id
+                            continue
+                        else:
+                            print("object sync error! bucket: {}, obj: {} with {}".format(bucket.id, obj_id, err))
+                            break
+        except Exception as err:
+            if last_bucket_id != bucket_id:
+                last_bucket_id = bucket_id
+                continue
+            else:
+                print("bucket sync error! bucket: {} with {}".format(bucket_id, err))
+                break
+
+
+if __name__ == '__main__':
+    main()
--- syncserver/sync.py
+++ syncserver/sync.py
@@ -1,10 +1,7 @@
 import os
 import sys
-from multiprocessing import Pool
 
 import django
-from celery.exceptions import SoftTimeLimitExceeded
-from tqdm import tqdm
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # 设置项目的配置文件 不做修改的话就是 settings 文件
@@ -14,59 +11,11 @@ django.setup()
 from api.backup import AsyncBucketManager
 from syncserver import celery_app
 
-manager = AsyncBucketManager()
-pool = Pool(os.cpu_count())
-
 
 @celery_app.task(
     bind=True,
-    autoretry_for=(SoftTimeLimitExceeded,),
-    retry_backoff=True,  # 避免重试时出现大量重复请求
-    retry_kwargs={'max_retries': 2}
 )
 def sync_object(self, bucket_id, object_id, bucket_name: str, object_key: str):
+    manager = AsyncBucketManager()
     manager.async_object(bucket_id, object_id, bucket_name, object_key)
-
-
-def main():
-    def sync(bucket_id, object_id, bucket_name, object_key):
-        sync_object.delay(bucket_id, object_id, bucket_name, object_key)
-
-    def safe_gen_sync_args():
-        bucket_id = 0
-        last_bucket_id = 0
-        while True:
-            try:
-                buckets = manager.get_need_async_bucket_queryset(bucket_id)
-                if not buckets:
-                    break
-                bucket_id = buckets[-1].id
-                for bucket in tqdm(buckets, desc="{}-{}".format(buckets[0].id, buckets[-1].id)):
-                    obj_id = 0
-                    last_obj_id = 0
-                    while True:
-                        try:
-                            objs = manager.get_need_async_objects_queryset(bucket.id, obj_id)
-                            if not objs:
-                                break
-                            obj_id = objs[-1].id
-                            for obj in tqdm(objs, desc="bucket: {}".format(str(bucket.id)), leave=False):
-                                sync_object.delay(bucket.id, obj.id, bucket.name, obj.na)
-                        except:
-                            if last_obj_id != obj_id:
-                                last_obj_id = obj_id
-                                continue
-                            else:
-                                print("object sync error! bucket: {}, obj: {}".format(bucket.id, obj_id))
-                                break
-            except:
-                if last_bucket_id != bucket_id:
-                    last_bucket_id = bucket_id
-                    continue
-                else:
-                    print("bucket sync error! bucket: {}".format(bucket_id))
-                    break
-
-
-if __name__ == '__main__':
-    main()
+    return
