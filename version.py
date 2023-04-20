2aac831107e2828ddb52fc7faf93b8c2cbb81bb2
wanghuang <wanghuangsuper@aliyun.com>
2023-04-20 16:49:49 +0800

    修改git 获取信息命令、调整"关于"界面

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ import datetime
 from django.utils.version import get_version
 
 
-VERSION = (1, 0, 0, 'final', 0)     # 'alpha', 'beta', 'rc', 'final'
+VERSION = (1, 0, 0, 'rc', 2)     # 'alpha', 'beta', 'rc', 'final'
 
 
 def get_git_changeset():
@@ -13,36 +13,28 @@ def get_git_changeset():
     if "__file__" not in globals():
         return None
     repo_dir = os.path.dirname(os.path.abspath(__file__))
-    git_tag_info = {}
+
     # %(*refname) %(*authorname) %(*authoremail) %(*authordate) %(*subject)
     get_tag = subprocess.run(
-        'git for-each-ref --count=3 --sort="-*authordate" --format="%(*refname) %(*authorname) %(*authoremail) '
-        '%(*authordate) " "refs/tags"',
+        "git for-each-ref --count=3 --sort='-taggerdate' "
+        "--format='%(refname:short) || %(taggerdate:format:%s) || %(*authorname) || %(*authoremail) || %(subject)'"
+        " refs/tags/*",
         capture_output=True,
         shell=True,
         cwd=repo_dir,
         text=True,
     )
     try:
-        cmd_output = get_tag.stdout.split('\n')
-        tag_list = [item.split(' ', 3) for item in cmd_output[0:3]]
-
-        for info in tag_list:
-            tag = info[0].lstrip('refs/tags/').rstrip('^{}')
-            t = datetime.datetime.strptime(info[3].rstrip(' +0800'), '%a %b %d %H:%M:%S %Y')
-            git_tag_info[tag] = [info[1], info[2], t]  # [作者,邮箱,时间]
-
-            get_tag_desc = subprocess.run(
-                f'git tag --sort="-*authordate" -n10 -l {tag}',
-                capture_output=True,
-                shell=True,
-                cwd=repo_dir,
-                text=True,
-            )
-            cmd_output_tag_doc = get_tag_desc.stdout.split('\n')
-            tag_doc_list = [" ".join(item.split()).replace("*", "").replace(tag, "")
-                            for item in cmd_output_tag_doc[0:len(cmd_output_tag_doc) -1]]
-            git_tag_info[tag].append(tag_doc_list)
+        cmd_output = get_tag.stdout
+        lines = cmd_output.split('\n')[0:3]
+        tz = datetime.timezone.utc
+        git_tag_info = []
+        for line in lines:
+            tag = line.split('||')
+            if len(tag) == 5:
+                tag[1] = datetime.datetime.fromtimestamp(int(tag[1]), tz=tz)
+                tag[4] = tag[4].replace('*', '\n*')
+                git_tag_info.append(tag)
 
     except Exception:
         return None
